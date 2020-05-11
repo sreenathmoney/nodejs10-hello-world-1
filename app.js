778d4654bc0726eb84cf62d4ad3884a8cb5ac4ba
sreenathmoney <50770425+sreenathmoney@users.noreply.github.com>
2020-05-11 17:50:56 +0530

    app.js file updated
    app.js updated

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.status(200).send('Hello world new entry').end();
+  res.status(200).send('welcome to wellmicity Hello world new entry').end();
 });
 
 module.exports = app
